af35ee7340b6f054d5c11bbcf6b7f0e48f1950eb
Jeff Jin <guoxingj@usc.edu>
2017-05-30 17:46:03 -0700

    Configure routes for About,Home,Search Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   moduleId:module.id,
   selector: 'my-app',
   templateUrl: `app.component.html`,
+  entryComponents: [SearchComponent, AboutComponent],
 })
 export class AppComponent  { name = 'Angular'; }
